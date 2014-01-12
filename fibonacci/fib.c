a45c17748b408220b843475e63fa7111c931bf24
Brandon Wamboldt <brandon.wamboldt@gmail.com>
2014-01-11 16:21:36 -0800

    [fib] Improving fib.c

--- fibonacci/fib.c
+++ fibonacci/fib.c
@@ -1,33 +1,24 @@
+/**
+ * Fibonacci Sequence
+ *
+ * Calculates n digits of the fibonacci sequence, where n is a command line
+ * argument to the program.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 
-int *calculateFibonacciSequence(int n, int start)
+int *calculateFibonacciSequence(int n)
 {
-  int i;
-
-  // Allocate memory for our fibonacci sequence from the heap
-  int *numbers = malloc(sizeof(int) * n);
-
-  // Need to calculate at least one number
   if (n <= 0) {
     return 0;
   }
 
-  // Can't start with a number under zero
-  if (start != 0 && start != 1) {
-    return 0;
-  }
+  int i = 0;
+  int *numbers = malloc(sizeof(int) * n);
+  numbers[0] = 0;
+  numbers[1] = 1;
 
-  // Get the seed values
-  if (start == 0) {
-      numbers[0] = 0;
-      numbers[1] = 1;
-  } else {
-      numbers[0] = 1;
-      numbers[1] = 1;
-  }
-
-  // Calculate the sequence
   for (i = 1; i < n; i++) {
     numbers[i + 1] = numbers[i] + numbers[i - 1];
   }
@@ -38,10 +29,8 @@ int *calculateFibonacciSequence(int n, int start)
 int main(int argc, char *argv[])
 {
   int i, n;
-  int start = 0;
   int *sequence;
 
-  // Check to see how many arguments we got
   if (argc <= 1) {
     puts("Please pass the number of iterations to run.");
     return 1;
@@ -50,15 +39,8 @@ int main(int argc, char *argv[])
   // Get the number of iterations to run
   n = atoi(argv[1]);
 
-  // Did they specify a start value
-  if (argc > 2) {
-    start = atoi(argv[2]);
-  }
+  sequence = calculateFibonacciSequence(n);
 
-  // Calculate the sequence
-  sequence = calculateFibonacciSequence(n, start);
-
-  // Output the sequence
   if (sequence) {
     printf("%d", sequence[0]);
 
@@ -66,13 +48,12 @@ int main(int argc, char *argv[])
       printf(",%d", sequence[i]);
     }
 
-    // Newline
-    puts("");
+    putchar('\n');
   } else {
     puts("Error processing fibonacci sequence");
+    return 1;
   }
 
-  // Free memory
   free(sequence);
 
   return 0;
