a0900d309bea06e00a7b5b1a2f93deb428935d81
Brandon Wamboldt <brandon.wamboldt@gmail.com>
2014-01-22 07:44:26 -0800

    SDL stuff

---
+++ sdl/c++/SDL_Tutorial_02/SDL_Tutorial_02/main.cpp
@@ -0,0 +1,82 @@
+/*
+ * Lesson 02 - Getting an Image on the Screen
+ *
+ * Now that you've already got a window open, let's put an image on it.
+ *
+ * http://lazyfoo.net/tutorials/SDL/02_getting_an_image_on_the_screen/index.php
+ */
+
+#include <SDL.h>
+#include <stdio.h>
+
+const int SCREEN_WIDTH = 800;
+const int SCREEN_HEIGHT = 640;
+
+SDL_Window *gWindow = nullptr;
+SDL_Surface *gScreenSurface = nullptr;
+SDL_Surface *gHelloWorld = nullptr;
+
+bool init()
+{
+	bool success = true;
+
+	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+		printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+		return false;
+	} else {
+		gWindow = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+
+		if (gWindow == nullptr) {
+			printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+			return false;
+		} else {
+			gScreenSurface = SDL_GetWindowSurface(gWindow);
+		}
+	}
+
+	return true;
+}
+
+bool loadMedia()
+{
+	gHelloWorld = SDL_LoadBMP("hello_world.bmp");
+
+	if (gHelloWorld == nullptr) {
+		printf("Unable to load image %s! SDL Error: %s\n", "hello_world.bmp", SDL_GetError());
+		return false;
+	}
+
+	return true;
+}
+
+void close()
+{
+	SDL_FreeSurface(gHelloWorld);
+	gHelloWorld = nullptr;
+
+	SDL_DestroyWindow(gWindow);
+	gWindow = nullptr;
+
+	SDL_Quit();
+}
+
+int main(int argc, char *argv[])
+{
+	if (!init()) {
+		printf("Failed to initialize!\n");
+	} else {
+		if (!loadMedia()) {
+			printf("Failed to load media!\n");
+		} else {
+			SDL_BlitSurface(gHelloWorld, NULL, gScreenSurface, NULL);
+
+			SDL_UpdateWindowSurface(gWindow);
+
+			SDL_Delay(2000);
+		}
+	}
+
+	close();
+
+	return 0;
+}
